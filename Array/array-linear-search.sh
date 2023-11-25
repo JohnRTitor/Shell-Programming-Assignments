b5f170a16b502adac4c68308a81313d4ef04413a
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-11-25 18:24:06 +0530

    Format fixes

--- Array/array-linear-search.sh
+++ Array/array-linear-search.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Get array input_array
-read -p "Enter space-separated numbers: " numbers
+read -p "Enter space-separated numbers: " -a numbers
 
+# Get the integer for which the user wants to search
 read -p "Enter the number to search: " search_for
 
 found=false
 position=1
 for number in ${numbers[*]}; do
     if (( number == search_for )); then
-        echo "$search_for found in $position'th position"
+        echo "$search_for found in #$position position"
         found=true
     fi
     position=$((position+1))
--- Array/array-size.sh
+++ Array/array-size.sh
@@ -5,8 +5,9 @@ read -p "Enter space-seperated numbers: " numbers
 
 # Determine the size
 size=0
-for number in ${numbers[*]}; do
-    size=$(($size + 1))
+for _ in ${numbers[*]}; do
+    size=$((size + 1))
 done
+# Equivalent to size=${#numbers[@]}
 
 echo "The size of the array is $size"
