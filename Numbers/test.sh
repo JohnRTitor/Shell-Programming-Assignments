0e686ce3f4e7395aff2bc4d57a72f6908f8d3dee
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-12-05 23:20:25 +0530

    file script ready

--- Numbers/test.sh
+++ Numbers/test.sh
@@ -1,31 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Function to check if a string is in kebab case
-is_kebab_case() {
+function is_kebab_case
+{
     local input="$1"
     # Check if the string matches kebab case pattern
     if [[ "$input" =~ ^[a-z]+(-[a-z]+)*$ ]]; then
-        echo "The file name is in kebab case."
+        echo true
     else
-        echo "The file name is not in kebab case."
+        echo false
     fi
 }
 
-# Take input as path
-read -p "Enter the file path: " file_path
+base_commit=$(git rev-parse HEAD~1)
+current_commit=$(git rev-parse HEAD)
 
-# Extract file name from the path
-file_name=$(basename "$file_path")
+changed_files=$(git diff --name-only $base_commit $current_commit)
 
-# Print the extracted file name
-echo "Extracted file name: $file_name"
+echo "Non-kebab case file names: "
+# Read the changed_files variable line by line
+# assign the current line to the variable file_path
+found=false
+echo "$changed_files" | while read -r file_path; do
+    # Extract file name from the path
+    file_name=$(basename "$file_path")
+    # Remove the extension from the file name
+    file_name="${file_name%.*}"
 
-file_name=$(basename "$file_path")
+    # Check if the file name is in kebab case
+    if [[ $(is_kebab_case $file_name) = false ]]; then
+        echo "$file_path"
+        found=true
+    fi
+done
 
-# Print the extracted file name
-echo "Extracted file name: $file_name"
-
-
-
-# Check if the file name is in kebab case
-is_kebab_case "$file_name"
+if [[ $found = false ]]; then
+    echo "None! Well done! :)"
+else
+    echo "Please fix the above files!"
+    return 1
+fi
