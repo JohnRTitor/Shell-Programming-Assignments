359bb996f6434faa0c9fc6e43379a24d14afd415
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-12-26 19:48:38 +0530

    Added insertion sort

---
+++ Array/array-insertion-sort.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+read -p "Enter space-seperated numbers: " -a arr
+
+printf "Original array: %s\n" "${arr[*]}"
+
+size=${#arr[*]}
+for (( i=1; i<size; i++ )); do
+    key=${arr[i]}
+    j=$(( i - 1 ))
+
+    while (( j >= 0 && arr[j] > key)); do
+        arr[j + 1]=${arr[j]}
+        j=$(( j - 1 ))
+    done
+    arr[j + 1]=$key
+done
+
+printf "Sorted array: %s \n" "${arr[*]}"
