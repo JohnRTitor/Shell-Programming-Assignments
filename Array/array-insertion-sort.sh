64009ed4ed09d198773e53673e30a1052fd60f95
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2023-12-26 19:49:50 +0530

    Minor bugfix

--- Array/array-insertion-sort.sh
+++ Array/array-insertion-sort.sh
@@ -9,7 +9,7 @@ for (( i=1; i<size; i++ )); do
     key=${arr[i]}
     j=$(( i - 1 ))
 
-    while (( j >= 0 && arr[j] > key)); do
+    while (( j >= 0 && arr[j] > key )); do
         arr[j + 1]=${arr[j]}
         j=$(( j - 1 ))
     done
