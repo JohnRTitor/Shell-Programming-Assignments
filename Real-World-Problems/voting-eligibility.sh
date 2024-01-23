8be43fecd2c57b3eb191b1f92790045dce00178a
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-01-23 18:33:16 +0530

    remove unnecessary $

--- Real-World-Problems/voting-eligibility.sh
+++ Real-World-Problems/voting-eligibility.sh
@@ -7,5 +7,5 @@ if (( age < 0 )); then
 elif (( age >= 18 )); then
     echo "You are eligible to vote."
 else
-    echo "You are not eligible to vote. please wait for $((18 - $age)) years."
+    echo "You are not eligible to vote. please wait for $((18 - age)) years."
 fi
