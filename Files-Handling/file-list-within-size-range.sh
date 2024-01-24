40bb9bb705ba6f8259ddf1c26541d8b2cec2a44c
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-01-24 21:42:08 +0530

    fix bugs

--- Files-Handling/file-list-within-size-range.sh
+++ Files-Handling/file-list-within-size-range.sh
@@ -2,7 +2,7 @@
 
 # Check if arguments are provided or not
 if [ $# -ne 2 ]; then
-    echo "Please provide the required arguments: min_size and max_size properly."
+    printf "Please provide the required arguments: min_size and max_size properly (in bytes). \n"
     exit 1
 fi
 
@@ -12,4 +12,4 @@ max_size=$2
 # Use find to get a list of files in the current directory within the specified size range
 file_list=$(find . -maxdepth 1 -type f -size +"$min_size"c -size -"$max_size"c -exec basename {} \;)
 
-echo $file_list | tr ' ' '\n'
+echo "$file_list" | tr ' ' '\n'
