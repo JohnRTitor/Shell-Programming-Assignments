c009cef1ad0edd401ae8f61f482e916660700bb1
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2024-01-27 20:23:55 +0530

    Move to printf

--- Array/array-binary-search.sh
+++ Array/array-binary-search.sh
@@ -3,19 +3,21 @@
 # Function to perform binary search
 function binary_search
 {
-    local target=$1
+    local -i target=$1
     local to_search=("${@:2}")
-    local length=${#to_search[@]}
 
-    local start=0
-    local end=$((length - 1))
+    local -i length=${#to_search[@]}
+    local -i start=0
+    local -i end=$((length - 1))
+
+    local -i middle middle_element
 
     while (( start <= end )); do
         middle=$((start + (end - start) / 2))
         middle_element=${to_search[middle]}
 
         if (( middle_element == target )); then
-            echo "Element $target found at position $((middle + 1))"
+            printf "Element %d found at position %d. \n" "$target" "$((middle + 1))"
             return 0
         elif (( middle_element < target )); then
             start=$((middle + 1))
@@ -24,16 +26,16 @@ function binary_search
         fi
     done
 
-    echo "Element $target not found in the array."
+    printf "Element %d not found in the array." "$target"
     return 1
 }
 
 # Read the sorted array from the user
-read -p "Enter a sorted array : " input
-IFS=' ' read -ra sortedArray <<< "$input"
+read -p "Enter a sorted array: " input
+IFS=' ' read -ra sorted_array <<< "$input"
 
 # Read the target element from the user
 read -p "Enter the element to be searched: " target
 
 # Perform binary search
-binary_search "$target" "${sortedArray[@]}"
+binary_search "$target" "${sorted_array[@]}"
